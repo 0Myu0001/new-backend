53692163d19b68459b404bcf913581009e900fe6
0Myu0001 <ktksyr193@gmail.com>
2024-05-30 20:58:22 +0900

    data can be deleted from the database using the following command

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets, routers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,116 +8,166 @@ from .serializers import *
 
 # Create your views here.
 
-class UserApi(viewsets.ModelViewSet):
+class UserApi (viewsets.ModelViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
+  lookup_field = 'user_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = User.objects.filter(user_id=pk)
-    user = get_object_or_404(queryset, user_id=pk)
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = User.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
 
 class User_FollowersApi (viewsets.ModelViewSet):
   queryset = User_Followers.objects.all()
   serializer_class = User_FollowersSerializer
+  lookup_field = 'user_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = User_Followers.objects.filter(user_id=pk)
-    user_followers = get_object_or_404(queryset, user_id=pk)
-    serializer = User_FollowersSerializer(user_followers)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = User_Followers.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
 
 class User_FollowingsApi (viewsets.ModelViewSet):
   queryset = User_Followings.objects.all()
   serializer_class = User_FollowingsSerializer
+  lookup_field = 'user_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = User_Followings.objects.filter(user_id=pk)
-    user_followings = get_object_or_404(queryset, user_id=pk)
-    serializer = User_FollowingsSerializer(user_followings)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = User_Followings.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
 
 class User_Strict_InformationApi (viewsets.ModelViewSet):
   queryset = User_Strict_Information.objects.all()
   serializer_class = User_Strict_InformationSerializer
+  lookup_field = 'user_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = User_Strict_Information.objects.filter(user_id=pk)
-    user_strict_information = get_object_or_404(queryset, user_id=pk)
-    serializer = User_Strict_InformationSerializer(user_strict_information)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = User_Strict_Information.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
 
 class PostApi (viewsets.ModelViewSet):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
+  lookup_field = 'post_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = Post.objects.filter(post_id=pk)
-    post = get_object_or_404(queryset, post_id=pk)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = Post.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
   
 class Post_CommentsApi (viewsets.ModelViewSet):
   queryset = Post_Comments.objects.all()
   serializer_class = Post_CommentsSerializer
+  lookup_field = 'post_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = Post.objects.filter(post_id=pk)
-    post = get_object_or_404(queryset, post_id=pk)
-    serializer = Post_CommentsSerializer(post)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = Post_Comments.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
   
 class Post_LovedApi (viewsets.ModelViewSet):
   queryset = Post_Loved.objects.all()
   serializer_class = Post_LovedSerializer
+  lookup_field = 'post_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = Post.objects.filter(post_id=pk)
-    post = get_object_or_404(queryset, post_id=pk)
-    serializer = Post_LovedSerializer(post)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = Post_Loved.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
 
 class PlaylistApi (viewsets.ModelViewSet):
   queryset = Playlist.objects.all()
   serializer_class = PlaylistSerializer
+  lookup_field = 'playlist_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = Post.objects.filter(playlist_id=pk)
-    playlist = get_object_or_404(queryset, playlist_id=pk)
-    serializer = PlaylistSerializer(playlist)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = Playlist.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
 
 class Playlist_CommentsApi (viewsets.ModelViewSet):
   queryset = Playlist_Comments.objects.all()
   serializer_class = Playlist_CommentsSerializer
+  lookup_field = 'playlist_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = Post.objects.filter(playlist_id=pk)
-    playlist = get_object_or_404(queryset, playlist_id=pk)
-    serializer = Playlist_CommentsSerializer(playlist)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = Playlist_Comments.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
 
 class Playlist_LovedApi (viewsets.ModelViewSet):
   queryset = Playlist_Loved.objects.all()
   serializer_class = Playlist_LovedSerializer
+  lookup_field = 'playlist_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = Post.objects.filter(playlist_id=pk)
-    playlist = get_object_or_404(queryset, playlist_id=pk)
-    serializer = Playlist_LovedSerializer(playlist)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = Playlist_Loved.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
   
 class NotificationApi (viewsets.ModelViewSet):
   queryset = Notification.objects.all()
   serializer_class = NotificationSerializer
+  lookup_field = 'user_id'
 
-  def retrieve(self, request, pk=None):
-    queryset = Post.objects.filter(notification_id=pk)
-    notification = get_object_or_404(queryset, notification_id=pk)
-    serializer = NotificationSerializer(notification)
-    return Response(serializer.data)
+  def get_queryset(self):
+    queryset = Notification.objects.all()
+    user_id = self.request.query_params.get('user_id')
+    if user_id is not None:
+      queryset = queryset.filter(user_id=user_id)
+    return queryset
+  
+def create_user(request):
+  if request.method == 'POST':
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+      # データをセッションに保存
+      request.session['user_data'] = serializer.validated_data
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+@login_required
+def complete_registration(request):
+  # セッションからデータを取得
+  user_data = request.session.get('user_data')
+  if user_data:
+    # データをモデルに保存
+    User.objects.create(**user_data)
+    # セッションからデータを削除
+    del request.session['user_data']
+    return Response({'status': 'Registration completed'}, status=status.HTTP_201_CREATED)
+  return Response({'status': 'No data found'}, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['POST'])
+def update_model(request):
+  serializer = UserSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
 router = routers.DefaultRouter()
 router.register(r'user', UserApi)
